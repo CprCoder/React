7aa1346f1d50fa22e9177a364bf0accf97f6f8c3
cprCoder <mr.Bakhtiyari.workplace@proton.me>
2023-06-12 07:06:46 +0330

    Finish filtered search-box

--- src/App.js
+++ src/App.js
@@ -8,10 +8,13 @@ class App extends Component {
     super();
     this.state = {
       monsters: [],
+      searchField: "",
     };
+    console.log("constructor");
   }
 
   componentDidMount() {
+    console.log("componentDidMount");
     fetch("https://jsonplaceholder.typicode.com/users")
       .then((Response) => Response.json())
       .then((users) =>
@@ -22,10 +25,25 @@ class App extends Component {
   }
 
   render() {
+    console.log("render");
+    const filteredMonsters = this.state.monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(this.state.searchField);
+    });
     return (
       <div className="App">
-        {this.state.monsters.map((monsters) => {
-          return <h1 key={monsters.id}>{monsters.name}</h1>;
+        <input
+          className="search-box"
+          placeholder="search monsters"
+          onChange={(event) => {
+            const searchField = event.target.value.toLocaleLowerCase();
+
+            this.setState(() => {
+              return { searchField };
+            });
+          }}
+        />
+        {filteredMonsters.map((monster) => {
+          return <h1 key={monster.id}>{monster.name}</h1>;
         })}
       </div>
     );
